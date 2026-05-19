03b760fcc5f68675dd763949d0254d3c40b70de7
Rian Putra Pratama <rianputrapratama666@gmail.com>
2026-05-19 10:55:12 +0700

    Menambahkan Workflow CI dan MLProject

---
+++ MLProject/modelling.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import mlflow
+import dagshub
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+mlflow.set_tracking_uri("mlruns")
+
+# Load Dataset Hasil Preprocessing
+current_dir = os.path.dirname(os.path.abspath(__file__))
+dataset_dir = os.path.join(current_dir, "wine_quality_preprocessing")
+
+X_train = pd.read_csv(os.path.join(dataset_dir, "X_train.csv"))
+y_train = pd.read_csv(os.path.join(dataset_dir, "y_train.csv")).values.ravel()
+X_test = pd.read_csv(os.path.join(dataset_dir, "X_test.csv"))
+y_test = pd.read_csv(os.path.join(dataset_dir, "y_test.csv")).values.ravel()
+
+# Manual Logging
+with mlflow.start_run(run_name="RandomForest_Manual_Logging"):
+    print("Memulai pelatihan model...")
+    
+    # Parameter Model
+    n_estimators = 100
+    random_state = 42
+    
+    # Melatih model
+    rf_model = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
+    rf_model.fit(X_train, y_train)
+    
+    # Prediksi & Evaluasi
+    y_pred = rf_model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    
+    # ================= MANUAL LOGGING =================
+    
+    # Log Parameter Model
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("random_state", random_state)
+    
+    # Log Metrik (Akurasi)
+    mlflow.log_metric("accuracy", accuracy)
+    
+    # Log Model Utama
+    mlflow.sklearn.log_model(rf_model, "model")
+    
+    # --- ARTEFAK TAMBAHAN 1: Confusion Matrix (Gambar) ---
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title("Confusion Matrix")
+    plt.ylabel("Actual")
+    plt.xlabel("Predicted")
+    plt.savefig("confusion_matrix.png")
+    mlflow.log_artifact("confusion_matrix.png")
+    
+    # --- ARTEFAK TAMBAHAN 2: Classification Report (Teks) ---
+    report = classification_report(y_test, y_pred)
+    with open("classification_report.txt", "w") as f:
+        f.write(report)
+    mlflow.log_artifact("classification_report.txt")
+    
+    print("Pelatihan selesai!")
